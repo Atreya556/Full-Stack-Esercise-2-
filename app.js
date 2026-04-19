b5b860f8acf8db674d147c850a294bfd203384f6
Dipan Sunar <deepanatreya18@gmail.com>
2026-04-19 23:05:52 +0300

    fix lint errors

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
-// get the port from env variable
-const PORT = process.env.PORT || 5001;
+app.use(express.static('dist'))
 
-app.use(express.static("dist"));
-
-const start = async () => {
-  await app.listen(PORT)
-  console.log(`server started on port ${PORT}`)
-}
-
-start()
+app.listen(8080, () => {
+  console.log('server started on port 8080')
+})
